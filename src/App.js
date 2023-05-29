471e995f0a28cc9a697ed21f6496d7c545bf7c62
Harshith Paladi <87645227+HarshithPaladi@users.noreply.github.com>
2023-05-29 17:42:15 +0530

    Add. simple counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { MyButton } from './components/MyButton';
+import { useState } from 'react';
 function App() {
+  const [counter, setCounter] = useState(0);
+  const increase = () => {
+    if (counter < 10) {
+      setCounter(count => count + 1);
+    }
+  };
+  const decrease = () => {
+    if (counter > 0) {
+      setCounter(count => count - 1);
+    }
+  };
+  const reset = () => {
+    setCounter(0);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //     <br/>
+    //     <button>Native ME!</button>
+    //     <MyButton/>
+    //   </header>
+    // </div>
+    <div className='counter'>
+      <h1>REACT COUNTER</h1>
+      <div className='counter_result'>
+        <span className='counter_output'>{counter}</span>
+        <div className='btn_container'>
+          <button className='control_btn' onClick={increase}>+</button>
+          <button className='control_btn' onClick={decrease}>-</button>
+          <button className='reset' onClick={reset}>Reset</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/MyButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function MyButton() {
+    return (
+        <div>
+            <button style={{ color: 'blue', fontSize:'2rem' }}>My Button</button>
+        </div>
+    );
+}
